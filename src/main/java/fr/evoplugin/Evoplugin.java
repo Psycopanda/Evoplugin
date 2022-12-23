bfa25838cc249ea730c66412ce0c6f1689aa1353
Av3nirr <82260202+Av3nirr@users.noreply.github.com>
2022-12-23 16:58:13 +0100

    Adding instance and welcome/goodbye message

--- src/main/java/fr/evoplugin/Evoplugin.java
+++ src/main/java/fr/evoplugin/Evoplugin.java
@@ -7,20 +7,27 @@ import fr.evoplugin.commands.CommandSite;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Evoplugin extends JavaPlugin {
-
+    //Création de l'instance
+    private static EvoPlugin INSTANCE;
+    
     @Override
     public void onEnable() {
+        System.out.println("Salut je démarre");
         setCommands();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println("Salut je m'en vais dans les tréfons des plugins éteins");
     }
     public void setCommands(){
         getCommand("broadcast").setExecutor(new CommandBroadcast());
         getCommand("discord").setExecutor(new CommandDiscord());
         getCommand("site").setExecutor(new CommandSite());
         getCommand("rules").setExecutor(new CommandRules());
+    }
+    //Retour de l'instance
+    public static EvoPlugin getInstance(){
+        return INSTANCE;
     }
 }
