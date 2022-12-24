c600d8e292c49a1810ab1ed5506d8c9d02c11b28
Av3nirr <82260202+Av3nirr@users.noreply.github.com>
2022-12-24 13:14:12 +0100

    Enlèvement de la variable dans le fichier commande spawn

--- src/main/java/fr/evoplugin/commands/CommandSpawn.java
+++ src/main/java/fr/evoplugin/commands/CommandSpawn.java
@@ -12,7 +12,6 @@ public class CommandSpawn implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player){
             Player p = (Player) sender;
-            Location spawn = new Location(p.getWorld(), 0, 60, 0, 1.8f, 10.5f);
             p.teleport(spawn);
         }
         return false;
